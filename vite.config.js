427a5519397729b1cdfb8cb58d181442d876137d
mo-alzainy <mo,alzainy@gmail.com>
2025-02-06 10:43:15 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
-    vue(), // Vue plugin
-    vueDevTools(), // Vue DevTools plugin (ensure you have this installed if you're using it)
+    vue(),
+
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // Resolves @ to the /src folder
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     fs: {
-      allow: ['..'], // Ensure that Vite allows access to parent directories
+      allow: ['..'],
     },
   },
 })
